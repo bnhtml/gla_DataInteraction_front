9f4fd7e1284d41e77e600586f355b1cb82c50911
bnhtml <1907905817@qq.com>
2019-01-22 18:11:18 +0800

    数据集成

--- src/views/work/datainteration/index.vue
+++ src/views/work/datainteration/index.vue
@@ -2,7 +2,10 @@
 <template>
 <div class='datainteration'>
   <el-card shadow="always" class="datainteration-header">
-    <p class="titleLeftBorder">数据集成操作指南</p>
+    <p class="titleLeftBorder">数据集成操作指南
+      <a href="#" class="right tool"><span class="iconfont icon-shangchuan"></span>下载数据集成工具</a>
+      <a href="#" class="right guide"><span class="iconfont icon-shangchuan"></span>下载数据集成操作指南</a>
+    </p>
     <div class="datainteration-table">
       <div class="database_box">
       <div class="source_database_text">源数据库</div>
@@ -54,12 +57,12 @@
   </el-card>
   <el-card shadow="always" class="datainteration-header">
     <p class="titleLeftBorder">操作步骤</p> 
-    <el-tabs class="datainteration-tabs" v-model="activeName2" type="card" @tab-click="handleClick">
+    <el-tabs class="datainteration-tabs" v-model="activeName2" type="border-card" @tab-click="handleClick">
       <el-tab-pane label="第一步" name="first">
         <h5>1.工具下载安装</h5>
         <br>
         <p>1.1 首先下载数据集成工具mt。解压压缩包migrationtool-1.0.6,双击migrationtool-1.0.6文件夹，进入到bin目录，双击starup.bat将迁移工具打开运行。</p>
-        <p>1.2 配置服务器host文件，host文件路径：C:\Windows\System32\drivers\etc\hosts,host文件配置：</p>
+        <p>1.2 配置服务器host文件，host文件路径：C:\Windows\System32\drivers\etc\hosts,host文件配置：<a href="#"><span class="iconfont icon-bianjiqi_chaolian"></span>hosts文件配置.txt</a></p>
         <br>
         <img src="../../../assets/images/step1-1.jpg" alt="">
         <br><br>
@@ -67,27 +70,70 @@
         <br>
         <p>如果迁移项目名称与之前已成功的迁移项目名称相同，则不能使用该名称，否则将出现警告：该迁移项目已存在。</p>
         <p>如果迁移项目名称与之前失败的迁移项目名称相同，则可以使用该名称，且迁移项目将覆盖之前未成功的迁移项目。</p>
-        <p>迁移开始后，EsgynDB迁移工具将在migrationtool-1.0.6 文件夹中创建esgyn 文件夹，并为每个迁移项目创建单独的饿文件夹。</p>
+        <p>迁移开始后，EsgynDB迁移工具将在migrationtool-1.0.6 文件夹中创建esgyn 文件夹，并为每个迁移项目创建单独的文件夹。</p>
         <br>
         <img src="../../../assets/images/step1-2.jpg" alt="">
       </el-tab-pane>
       <el-tab-pane label="第二步" name="second">
         <h5>1.输入源库的服务器信息</h5>
+        <br>
         <p>数据源选项可以选择其他类型的数据库，包括主流数据库mysql,oracle,postgresql,sql server和Esgyndb。填写完信息后，点击下一步。</p>
-        <img src="" alt="">
+        <br>
+        <img src="../../../assets/images/step2-1.jpg" alt="">
+        <br><br>
         <h5>2.输入目标库信息</h5>
+        <br>
         <p>该目标库目前只支持Esgyndb数据库，输入服务器地址，租户名称(大写)，用户名和密码后点击下一步继续。</p>
-        <img src="" alt="">
+        <br>
+        <img src="../../../assets/images/step2-2.jpg" alt="">
       </el-tab-pane>
       <el-tab-pane label="第三步" name="third">
         <h5>选择数据类型映射</h5>
         <br>
         <p>因为源库是MySQL数据库，将数据迁移集成到Esgyndb数据库时，不免要将一些数据类型进行转换。其中较为常见的为datatime转换为timestamp类型，text则推荐转换为varchar类型，Year类型推荐转换为smallint类型等。将类型转换完毕后点击下一步继续。</p>
+        <br>
+        <img src="../../../assets/images/step3-1.jpg" alt="">
       </el-tab-pane>
-      <el-tab-pane label="第四步" name="fourth">第四步</el-tab-pane>
-      <el-tab-pane label="第五步" name="fifth">第五步</el-tab-pane>
-      <el-tab-pane label="第六步" name="sixth">第六步</el-tab-pane>
-      <el-tab-pane label="第七步" name="seventh">第七步</el-tab-pane>
+      <el-tab-pane label="第四步" name="fourth">
+        <h5>选择源库的Schema/库/表空间</h5>
+        <br>
+        <p>选择要迁移数据的Schema/表/表空间</p>
+        <br>
+        <img src="../../../assets/images/step4-1.jpg" alt="">
+      </el-tab-pane>
+      <el-tab-pane label="第五步" name="fifth">
+          <h5>选择要迁移的表</h5>
+          <br>
+          <p>1.选择源库中需要迁移的表</p>
+          <p>2.输入目标表的schemas</p>
+          <p>3.选择只创建表或者只集成数据等</p>
+          <p>选择下一步继续</p>
+          <br>
+          <img src="../../../assets/images/step5-1.jpg" alt="">
+      </el-tab-pane>
+      <el-tab-pane label="第六步" name="sixth">
+          <h5>选择列</h5>
+          <br>
+          <p>如图所示，类型列中的数据类型是可以根据现实情况进行调换的，时间类型的默认值比较容易出现报错情况，需要注意。</p>
+          <p>如果时间有默认值，如图中所示，默认值需要更改为timestamp(‘1900-01-01 01:01:01’)。</p>
+          <br>
+          <img src="../../../assets/images/step6-1.jpg" alt="">
+      </el-tab-pane>
+      <el-tab-pane label="第七步" name="seventh">
+          <h5>1.选择其他对象</h5>
+          <br>
+          <p>此时是当表有其他外键表时的选项。</p>
+          <p>请注意左上角表一行的输入是否有错误。此时这个位置填写的内容应为目标schemas和目标表。完成后点击下一步继续</p>
+          <br>
+          <img src="../../../assets/images/step7-1.jpg" alt="">
+          <br>
+          <br>
+          <h5>2.迁移执行</h5>
+          <br>
+          <p>确认字段对应无误后点击下一步执行。</p>
+          <br>
+          <img src="../../../assets/images/step7-2.jpg" alt="">
+      </el-tab-pane>
     </el-tabs>
   </el-card>
 </div>
@@ -147,6 +193,7 @@ padding: 0px;
 .titleLeftBorder{
         padding-left: 14px;
         margin-left: -20px;
+        font-size: 16px;
 }    
 .database_box {
         display: flex;
@@ -222,6 +269,30 @@ padding: 0px;
         align-items: center;
 }
 img{
-  width: 100%;
+  /* width: 100%; */
+  max-width: 700px;
+}
+a{
+  color: #58CAFD;
+  font-size: 14px;
+}
+span,p{
+  font-size: 14px;
+}
+h5{
+  font-size: 16px;
+}
+.tool{
+  margin-right: 170px;
+}
+.guide{
+  margin-right: 20px;
+}
+el-tabs{
+  /* background-color: #EFF2F5; */
+  background-color: #fff;
+}
+.datainteration /deep/ .el-tabs__nav-wrap {
+    padding-left: 0px !important;
 }
 </style>
