08232a033f79f3524e58d41cc87fca14c69ea821
仓真真 <1363359649@qq.com>
2019-01-24 11:14:31 +0800

    fz12

--- src/views/work/dataUpperShelf/apiInfo/params.vue
+++ src/views/work/dataUpperShelf/apiInfo/params.vue
@@ -42,7 +42,7 @@
       init() {
         this.isShow = false;
         this.tableJson = this.tableQuery.tableJson;
-        this.url = `${this.SERVER_BASE_URL}/new_interface/api_desc`;
+        this.url = `${this.$SERVER_BASE_URL}/new_interface/api_desc`;
         this.$nextTick(() => {
           this.isShow = true;
         })
